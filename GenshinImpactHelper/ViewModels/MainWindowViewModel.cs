fb3280f82f0ae2c3f126a465789555dc5910c268
Matrixoid <mr.art.pro@mail.ru>
2023-11-11 23:58:46 +0300

    Добавлен заголовок окна

--- GenshinImpactHelper/ViewModels/MainWindowViewModel.cs
+++ GenshinImpactHelper/ViewModels/MainWindowViewModel.cs
@@ -4,6 +4,18 @@ namespace GenshinImpactHelper.ViewModels
 {
     internal class MainWindowViewModel : ViewModel
     {
+        #region Заголовок окна
+        private string _title;
 
+        public string Title {
+            get => _title;
+            set => Set(ref _title, value);
+        }
+        #endregion
+
+        public MainWindowViewModel()
+        {
+            Title = "GenshinImpactHelper";
+        }
     }
 }
