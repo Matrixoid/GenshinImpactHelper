36f3c85413dabba8c171d9cf341f6796b4058faa
Matrixoid <mr.art.pro@mail.ru>
2023-11-12 19:34:27 +0300

    Удалены лишние using'и

--- GenshinImpactHelper/ViewModels/AppWindowViewModel.cs
+++ GenshinImpactHelper/ViewModels/AppWindowViewModel.cs
@@ -1,9 +1,4 @@
 ﻿using GenshinImpactHelper.ViewModels.Base;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace GenshinImpactHelper.ViewModels
 {
