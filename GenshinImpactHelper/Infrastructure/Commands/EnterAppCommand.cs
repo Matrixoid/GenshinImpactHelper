3a99dc9da5d520455ea76667ea0de39ef3b4bb9e
Matrixoid <mr.art.pro@mail.ru>
2023-11-12 17:52:34 +0300

    Реализована кнопка "Вход"

---
+++ GenshinImpactHelper/Infrastructure/Commands/EnterAppCommand.cs
@@ -0,0 +1,19 @@
+﻿using GenshinImpactHelper.Infrastructure.Commands.Base;
+using GenshinImpactHelper.Views.Windows;
+using System.Windows;
+
+namespace GenshinImpactHelper.Infrastructure.Commands
+{
+    internal class EnterAppCommand : Command
+    {
+        public override bool CanExecute(object parameter) => true;
+
+        public override void Execute(object parameter)
+        {
+            var mainWindow = Application.Current.MainWindow;
+            mainWindow.Hide();
+            AppWindow appWindow = new AppWindow();
+            appWindow.Show();
+        }
+    }
+}
---
+++ GenshinImpactHelper/ViewModels/AppWindowViewModel.cs
@@ -0,0 +1,27 @@
+﻿using GenshinImpactHelper.ViewModels.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenshinImpactHelper.ViewModels
+{
+    internal class AppWindowViewModel : ViewModel
+    {
+        #region Заголовок окна
+        private string _title;
+
+        public string Title
+        {
+            get => _title;
+            set => Set(ref _title, value);
+        }
+        #endregion
+
+        public AppWindowViewModel()
+        {
+            Title = "GenshinImpactHelper";
+        }
+    }
+}
