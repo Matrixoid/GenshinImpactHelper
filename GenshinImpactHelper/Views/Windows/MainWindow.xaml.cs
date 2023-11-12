ee61cf102537181600c79d4a40a68b33b0a45439
Matrixoid <mr.art.pro@mail.ru>
2023-11-12 16:58:21 +0300

    Добавлена анимация для изображений персонажей и для лого

--- GenshinImpactHelper/Views/Windows/MainWindow.xaml.cs
+++ GenshinImpactHelper/Views/Windows/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -9,6 +10,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -23,6 +25,29 @@ namespace GenshinImpactHelper.Views.Windows
         public MainWindow()
         {
             InitializeComponent();
+
+            TranslateTransform trans1 = new TranslateTransform();
+            trans1.X -= 100;
+            amber.RenderTransform = trans1;
+
+            DoubleAnimation amberAnimation = new DoubleAnimation(-100, 0, TimeSpan.FromSeconds(2));
+            trans1.BeginAnimation(TranslateTransform.XProperty, amberAnimation);
+
+            TranslateTransform trans2 = new TranslateTransform();
+            trans2.X += 160;
+            lisa.RenderTransform = trans2;
+
+            DoubleAnimation lisaAnimation = new DoubleAnimation(160, 0, TimeSpan.FromSeconds(2));
+            trans2.BeginAnimation(TranslateTransform.XProperty, lisaAnimation);
+
+            TranslateTransform trans3 = new TranslateTransform();
+            trans3.Y -= 110;
+            logo.RenderTransform = trans3;
+
+            DoubleAnimation logoAnimation = new DoubleAnimation(-110, 0, TimeSpan.FromSeconds(1.5));
+            logoAnimation.BeginTime = TimeSpan.FromSeconds(2);
+            trans3.BeginAnimation(TranslateTransform.YProperty, logoAnimation);
+
         }
     }
 }
